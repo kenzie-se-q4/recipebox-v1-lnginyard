9bb4ce7a6cd3b4907695c85458646907e30a155b
Lorenzo Ginyard <lnginyard@gmail.kenzie.academy.com>
2021-10-24 16:15:44 -0400

    added forms to app

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -1,43 +1,51 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
 from recipe_app.models import Author, Recipe
 from recipe_app.forms import AuthorForm, RecipeForm
 
 
 def index(request):
-    recipe_view = Recipe.objects.all()
-    return render(request, 'index.html', {'recipes': recipe_view})
+    recipes = Recipe.objects.all()
+    return render(request, 'index.html', {'recipes': recipes})
 
 
 def add_author(request):
-    form = AuthorForm(request.POST, request.FILES)
-    return render(request, 'author.html', {'form': form})
+    if request.method == "POST":
+        form = AuthorForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            Author.objects.create(
+                name = data['name'],
+                bio = data['bio']
+            )
+        return HttpResponseRedirect(reverse('Home'))
+    form = AuthorForm()
+    return render(request, 'add_author.html', {'form': form})
 
 
 def add_recipe(request):
     if request.method == "POST":
         form = RecipeForm(request.POST)
-        if form.is_vaid():
+        if form.is_valid():
             data = form.cleaned_data
-            author _ Recipe.objects.create(
-                title=data.get("title")
-                author=data.get("author")
-                description=data.get("description"),
-                time_required=data.get("time_required"),
-                instructions=data.get("instructions"),
+            Recipe.objects.create(
+                title=data["title"],
+                author=data["author"],
+                description=data["description"],
+                cook_time=data["cook_time"],
+                instructions=data["instructions"],
             )
-            return HttpResponseRedirect(reverse("homepage")) 
-else:
-    form = AddRecipeForm()
-
-return render(request, "", {"form": form})
+        return HttpResponseRedirect(reverse("Home"))
+    form = RecipeForm()
+    return render(request, "add_recipe.html", {"form": form})
 
 
-def recipe_detail(request, recipe_id):
-    recipe = Recipe.objects.get(id=recipe_id)
+def recipe_detail(request, id):
+    recipe = Recipe.objects.get(id=id)
     return render(request, 'recipe_detail.html', {'recipe': recipe})
 
 
-def auth_deets(request, author_id):
-    author = Author.objects.get(id=author_id)
+def auth_deets(request, id):
+    author = Author.objects.get(id=id)
     recipe = Recipe.objects.filter(author=author)
-    return render(request, 'author_detail.html', {'author': author, 'recipe': recipe})
+    return render(request, 'author_detail.html', {'author': author,
+                                                  'recipe': recipe})
